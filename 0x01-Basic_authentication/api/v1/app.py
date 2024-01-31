317ceb080e8061fa896cf065589409337e3278e4
browniwils <browniwils@gmail.com>
2024-01-31 11:39:29 +0000

    Fix blank line pycodestyle

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -36,7 +36,7 @@ def not_found(error) -> str:
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """Unauthorized handler."""
-    
+
     return jsonify({"error": "Unauthorized"}), 401
 
 
